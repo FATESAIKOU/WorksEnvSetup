9c56637597f13d459e8c571111699f6a7e742de1
FATESAIKOU <qzsecftbhhhh@gmail.com>
2020-07-19 03:09:24 +0900

    Speedup clean.sh and run.sh for all env

--- envs/python3/run.sh
+++ envs/python3/run.sh
@@ -6,9 +6,11 @@ command=$2
 container=$(docker run \
     -u root \
     -i \
+    -t \
     -d \
     --rm \
     -v $project_url:/project \
-    python3)
+    python3 bash)
 
 docker exec -it $container $command
+docker stop $container > /dev/null
